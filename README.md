340da685c869f17752dd129776f01b949a6f2003
ahimmelstoss <ahimmelstoss@gmail.com>
2014-11-24 20:32:45 -0500

    more vids

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Dog Time
 
-Dog Time gives an amusing playful dog video with each new tab load, along with some helpful information.
+Dog Time gives an amusing playful dog video with each new tab load, along with some helpful information. Based off of the popular [Cat Therapy](https://chrome.google.com/webstore/detail/cat-therapy/iimbcdloghfgakpflgabmkaocfkpdkfm) Chrome Extension.
 
 To download the the Chrome Extension, click [here]().
--- public/js/video.js
+++ public/js/video.js
@@ -9,10 +9,19 @@ var dogVideos = [
   "http://media.giphy.com/media/12b3E4U9aSndxC/giphy.mp4",
   "http://media.giphy.com/media/8Ow0GB3YJ7HFe/giphy.mp4",
   "http://media.giphy.com/media/cBTuHHvv5iqac/giphy.mp4",
-  "http://media.giphy.com/media./wG3jhHKvoCCVG/giphy.mp4",
+  "http://media.giphy.com/media/wG3jhHKvoCCVG/giphy.mp4",
   "http://media.giphy.com/media/yoJC2LTvmB70IltllK/giphy.mp4",
   "http://media.giphy.com/media/DIrGyd84DwA48/giphy.mp4",
-  "http://media.giphy.com/media/UIIyMCABbnUAg/giphy.mp4"
+  "http://media.giphy.com/media/UIIyMCABbnUAg/giphy.mp4",
+  "http://media.giphy.com/media/uJG2A0WvErkGY/giphy.mp4",
+  "http://media.giphy.com/media/mWCXHLTASb9W8/giphy.mp4",
+  "http://media.giphy.com/media/keBpkd1tOgOac/giphy.mp4",
+  "http://media.giphy.com/media/d2vtMFc2YTVjW/giphy.mp4",
+  "http://media.giphy.com/media/EPkb6xoNOR1Sg/giphy.mp4",
+  "http://media.giphy.com/media/A6XISxDsthHGw/giphy.mp4",
+  "http://media.giphy.com/media/vGo2sgzeC8r60/giphy.mp4",
+  "http://media.giphy.com/media/JxuBBMH9gcJt6/giphy.mp4",
+  "http://media.giphy.com/media/cIQkxw4G65TG/giphy.mp4"
 ]
 
 function displayRandomVideo() {
