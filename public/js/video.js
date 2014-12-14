40f1b3ff71fe010c198589aefa8f00f447acd7af
ahimmelstoss <ahimmelstoss@gmail.com>
2014-12-14 12:58:30 -0500

    1.2

--- public/js/video.js
+++ public/js/video.js
@@ -21,7 +21,8 @@ var dogVideos = [
   "http://media.giphy.com/media/A6XISxDsthHGw/giphy.mp4",
   "http://media.giphy.com/media/vGo2sgzeC8r60/giphy.mp4",
   "http://media.giphy.com/media/JxuBBMH9gcJt6/giphy.mp4",
-  "http://media.giphy.com/media/cIQkxw4G65TG/giphy.mp4"
+  "http://media.giphy.com/media/cIQkxw4G65TG/giphy.mp4",
+  "http://media.giphy.com/media/5xaOcLSBjc8W1uoMq8E/giphy.mp4"
 ];
 
 function displayRandomVideo() {
